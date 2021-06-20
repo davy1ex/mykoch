d755d45b5fcc8d6168b3e4aa93c482ed40224917
dezy1ex <dezy1ex@gmail.com>
2021-06-20 15:32:47 +0400

    added dynamic draw workouts

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+const WORKOUT_GROUNDS = [
+    {id: 1, title: 'Workout 1', geo: '55\' 42\' 342\''},
+    {id: 2, title: 'Workout 2', geo: '145\' 32\' 342\''},
+    {id: 3, title: 'Workout 3', geo: '55\' 235\' 342\''}
+]
+
+
+app.get('/api/get_workouts', (req, res) => {
+    res.status(200).json(WORKOUT_GROUNDS)
+})
+
+
+app.use(express.static(path.resolve(__dirname, 'client')))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'index.html'))
+})
+
+app.listen(3000, () => console.log('im start'))
---
+++ client/js/frontend.js
@@ -0,0 +1,34 @@
+import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.esm.browser.js'
+
+new Vue({
+    el: '#app',
+    data() {
+        return {workouts: []}
+
+        
+    },
+    
+
+    async mounted() {
+        this.workouts = await request('/api/get_workouts')
+        console.log(this.workouts)
+        this.loading = false
+    }
+})
+
+async function request(url, method = 'GET', data = null) {
+    try {
+        const headers = {}
+        let body
+
+        const response = await fetch(url, {
+            method,
+            headers,
+            body
+        })
+        return await response.json()
+    }
+    catch (e) {
+        console.warn('Error:', e.message)
+    }
+}
